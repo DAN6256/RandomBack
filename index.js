62ccbfab8399373546d2db1be5b937bce2d6bf4c
Daniel Tunyinko <122823511+Tunyinko@users.noreply.github.com>
2025-03-06 18:12:02 +0000

    Added another fake end point to test

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { connectDB } = require('./src/config/db');
 const setupSwagger = require('./src/config/swagger'); 
 const authRoutes = require('./src/routes/auth.routes'); 
-const userRoutes = require('./src/routes/user.routes');
-const borrowRequestRoutes = require('./src/routes/borrowRequest.routes');
-const equipmentRoutes = require('./src/routes/equipment.routes');
+//const userRoutes = require('./src/routes/user.routes');
+//const borrowRequestRoutes = require('./src/routes/borrowRequest.routes');
+//const equipmentRoutes = require('./src/routes/equipment.routes');
 
 const app = express();
 const port = 3000;
@@ -13,9 +13,9 @@ app.use(express.json());
 
 // Register API routes
 app.use('/api/auth', authRoutes); 
-app.use('/api/users',userRoutes);
-app.use('/api/borrowRequest',borrowRequestRoutes);
-app.use('/api/equipments',equipmentRoutes);
+//app.use('/api/users',userRoutes);
+//app.use('/api/borrowRequest',borrowRequestRoutes);
+//app.use('/api/equipments',equipmentRoutes);
 
 // Setup Swagger UI 
 setupSwagger(app);
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /**
  * @swagger
- * /auth/login:
+ * /api/auth/login:
  *   post:
  *     summary: User login
  *     description: Authenticate user and return a JWT token.
@@ -40,6 +40,46 @@ const router = express.Router();
  */
 router.post('/login', (req, res) => {
     res.json({ token: 'fake-jwt-token' });
+});
+
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: User Signup
+ *     description: Signup user and allow  them to verify email.
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@ashesi.edu.gh
+ *               password:
+ *                 type: string
+ *                 example: Password123
+ *     responses:
+ *       201:
+ *         description: Successful creation of account
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *       401:
+ *         description: Unauthorized - Invalid credentials
+ */
+router.post('/signup', (req, res)=>{
+    res.json({message: 'Signed up'});
 });
 
 module.exports = router;
