e98de56569aabb6d3567635ff784f5c90828f24a
Daniel Tunyinko <122823511+Tunyinko@users.noreply.github.com>
2025-03-08 02:08:34 +0000

    Changed url during deployment

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -11,8 +11,8 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
-      description: 'Local development server'
+      url: 'https://backendservice-9fbf.onrender.com/',
+      description: 'Development test server'
     }
   ],
   components: {
@@ -35,7 +35,7 @@ const swaggerSpec = swaggerJSDoc(options);
 
 const setupSwagger = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log('Swagger UI available at http://localhost:3000/api-docs');
+  console.log('Swagger UI available https://backendservice-9fbf.onrender.com/api-docs');
 };
 
 module.exports = setupSwagger;
